574a89cc07e2212edd07696923a6149b97963361
JamesCaddick <57677871+JamesCaddick@users.noreply.github.com>
2023-12-02 23:03:59 +0000

    day 2 was easy

---
+++ Day 2/day_2_jac.py
@@ -0,0 +1,21 @@
+import re
+
+with open('input_jac.txt') as f:
+    data = [d.strip().split(':')[-1].split(';') for d in f.readlines()]
+    
+r = 12
+g = 13
+b = 14
+
+pt1 = 0
+pt2 = 0
+for i, d in enumerate(data):
+    reds = max([int(n) for n in re.findall(r'(\d+) red', ' '.join(d))])
+    greens = max([int(n) for n in re.findall(r'(\d+) green', ' '.join(d))])
+    blues = max([int(n) for n in re.findall(r'(\d+) blue', ' '.join(d))])
+    pt2 += reds * blues * greens
+    if reds <= r and greens <= g and blues <= b:
+        pt1 += i + 1
+
+print(pt1)
+print(pt2)
