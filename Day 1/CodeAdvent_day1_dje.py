43c0902015a4e7ceaa676e9fe524a3d3208daa12
Kyokcha <79158285+Kyokcha@users.noreply.github.com>
2023-12-15 13:28:00 +0000

    upload of the day 1 solution

---
+++ Day 1/CodeAdvent_day1_dje.py
@@ -0,0 +1,25 @@
+import re
+import numpy as np
+from icecream import ic
+
+#test inputs
+
+input_list = []
+
+total = 0
+
+file_path = 'input_data_day1_dje.txt'
+with open(file_path, 'r') as file:
+    for line in file:
+        input_list.append(line.strip())
+
+for row in input_list:
+    numbers = re.findall(r'\d+', row)
+    first_num = numbers[0][0]
+    last_num = numbers[-1][-1]
+    output = first_num + last_num
+    output = int(output)
+    total = total + output
+    ic(total)
+
+ic(total)
