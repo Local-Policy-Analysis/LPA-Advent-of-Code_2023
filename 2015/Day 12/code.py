90a2c524ac36783ac3d0a7fc29740e998846c9e1
JamesCaddick <57677871+JamesCaddick@users.noreply.github.com>
2024-12-07 14:32:53 +0000

    that was a struggle

--- 2015/Day 12/code.py
+++ 2015/Day 12/code.py
@@ -6,3 +6,37 @@ with open('input_jac.txt') as f:
 negatives = sum([int(n) for n in re.findall('-[0-9]+', data)])
 positives = sum([int(n) for n in re.findall('[^-|0-9]([0-9]+)', data)])
 print(negatives + positives)
+
+# if encounter a second opening brace before a red flag then reset opening brace count and
+# remove last element from starts
+
+open_brackets = 0
+close_brackets = 0
+red_flag = False
+starts = []
+ends = []
+reds = []
+red_flag = False
+for x in range(len(data)):
+    if data[x] == '{' and  open_brackets == 0:
+        open_brackets += 1
+        starts.append(x)
+    elif data[x] == '{' and  open_brackets != 0:
+        open_brackets += 1
+    elif data[x] == '}' and close_brackets + 1 == open_brackets:
+        ends.append(x)
+        reds.append(red_flag)
+        open_brackets = 0
+        close_brackets = 0
+        red_flag = False
+    elif data[x] == '}' and close_brackets + 1 != open_brackets:
+        close_brackets += 1
+    elif data[x:x+6] == ':"red"':
+        red_flag = True
+    
+pt2 = 0        
+for s, e, r in zip(starts, ends, reds):
+    if r:
+        pt2 += sum([int(n) for n in re.findall('-[0-9]+', data[s:e])]) + sum([int(n) for n in re.findall('[^-|0-9]([0-9]+)', data[s:e])])
+
+print(negatives + positives - pt2)    
---
+++ 2024/Day 6/code_jac.py
@@ -0,0 +1,58 @@
+import sys
+import numpy as np
+
+with open('input_jac.txt') as f:
+    data = np.array([[list(r) for r in d.strip()] for d in f.readlines()])[:, :, 0]
+    
+x_start, y_start = np.nonzero(data == '^')
+x_start = x_start[0]
+y_start = y_start[0]
+
+change_direction = {'up': 'right',
+                    'right': 'down',
+                    'down': 'left',
+                    'left': 'up'}
+steps = {'up': (-1, 0),
+         'down': (1, 0),
+         'left': (0, -1),
+         'right': (0, 1)}
+
+x = x_start
+y = y_start
+obstacles = set()
+temp_data = data.copy()
+direction = 'up'
+while x < temp_data.shape[0] and x >= 0 and y < temp_data.shape[1] and y >= 0:
+    if temp_data[x, y] == '#':
+        x -= steps[direction][0]
+        y -= steps[direction][1]
+        direction = change_direction[direction]
+    else:
+        obstacles.add((x, y))
+        x += steps[direction][0]
+        y += steps[direction][1]
+
+print(len(obstacles))
+
+pt2 = 0
+for o in obstacles:
+    x = x_start
+    y = y_start
+    direction = 'up'
+    visited = set()
+    temp_data = data.copy()
+    temp_data[o] = '#'
+    while x < temp_data.shape[0] and x >= 0 and y < temp_data.shape[1] and y >= 0:
+        if (x, y, direction) in visited:
+            pt2 += 1
+            break
+        if temp_data[x, y] == '#':
+            x -= steps[direction][0]
+            y -= steps[direction][1]
+            direction = change_direction[direction]
+        else:
+            visited.add((x, y, direction))
+            x += steps[direction][0]
+            y += steps[direction][1]
+            
+print(pt2)
