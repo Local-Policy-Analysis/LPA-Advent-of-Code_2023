6b1823053d6867feb60b96668988f27db321f944
JamesCaddick <57677871+JamesCaddick@users.noreply.github.com>
2025-01-10 09:01:50 +0000

    day 19

--- 2024/Day 19/code_jac.py
+++ 2024/Day 19/code_jac.py
@@ -3,9 +3,31 @@ with open('input_jac.txt') as f:
     
 patterns = patterns.split(', ')
 designs = designs.split('\n')
-    
-valid = 0
+
+memo = {}
+def backtrack(design):
+    if design not in memo:
+        if not design:
+            return 1
+        else:
+            solutions = 0
+        candidates = [p for p in patterns if len(p) <= len(design)]
+        for candidate in candidates:
+            if design[:len(candidate)] != candidate:
+                continue
+            solutions += backtrack(design[len(candidate):])
+        memo[design] = solutions
+    return memo[design]
+
+pt1 = 0
+pt2 = 0
 for design in designs:
-    valid += 1
+    used = []
+    valid = backtrack(design)
+    if valid:
+        pt1 += 1
+        pt2 += valid
+        
+print(pt1)
+print(pt2)
     
-print(valid)
